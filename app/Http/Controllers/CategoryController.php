63f0392f79373b603b803b6e466274b807656a94
dipras <dipras75@gmail.com>
2019-07-13 15:03:00 +0700

    CM21:content category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use function GuzzleHttp\json_decode;
+
+class CategoryController extends Controller
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    public function list($id = null)
+    {
+        if (is_null($id)) {
+            $result = $this->client->request('GET', 'http://private-anon-4dd6595d8c-dbil.apiary-mock.com/content/category');
+            if ($result->getStatusCode() != 200) {
+                return response()->json([
+                    'status' => [
+                        'code' => '500',
+                        'message' => 'Bad Gateway',
+                    ]
+                ], 500);
+            } else {
+                return response()->json(json_decode($result->getBody(), true), 200);
+            }
+        } else {
+            $content = $this->client->request('GET', 'http://private-2e0bb9-dbil.apiary-mock.com/content/metadata');
+            if ($content->getStatusCode() != 200) {
+                return response()->json([
+                    'status' => [
+                        'code' => '500',
+                        'message' => 'Bad Gateway',
+                    ]
+                ], 500);
+            }
+            $data = json_decode($content->getBody(), true);
+            $data = ($data['result']);
+            $i = 0;
+            $kode = null;
+            foreach ($data as $d) {
+                if ($d['category_id'] == $id) {
+                    $kode[$i] = $d;
+                    $i++;
+                }
+            }
+
+            if (is_null($kode)) {
+                return response()->json([
+                    'status' => [
+                        'code' => '404',
+                        'message' => 'Not Found',
+                    ]
+                ], 404);
+            }
+
+            return response()->json($kode, 200);
+        }
+    }
+
+    public function search_videos(Request $request, $genre = null)
+    {
+        if (is_null($genre)) {
+            $aturan = [
+                'title' => 'required'
+            ];
+            $pesan = [
+                'required' => 'Please Fill Attribute :attribut'
+            ];
+            $this->validate($request, $aturan, $pesan);
+
+            $data = $this->client->request('POST', 'http://private-2e0bb9-dbil.apiary-mock.com/content/metadata/search', [
+                'query' => urlencode('"video_title=' . $request->title . '"')
+            ]);
+            if ($data->getStatusCode() != 200) {
+                return response()->json([
+                    'code' => '500',
+                    'message' => 'Bad Gateway'
+                ], 500);
+            }
+
+            $result = json_decode($data->getBody(), true);
+
+            if ($result['status']['total'] < 1) {
+                return response()->json([
+                    'code' => '404',
+                    'message' => 'Not found'
+                ], 404);
+            }
+
+            return response()->json($result, 200);
+        } else {
+            $data = $this->client->request('POST', 'http://private-2e0bb9-dbil.apiary-mock.com/content/metadata/search', [
+                'query' => urlencode('"video_genre=' . $genre . '"')
+            ]);
+
+            if ($data->getStatusCode() != 200) {
+                return response()->json([
+                    'code' => '500',
+                    'message' => 'Bad Gateway'
+                ], 500);
+            }
+
+            $result = json_decode($data->getBody(), true);
+
+            if ($result['status']['total'] < 1) {
+                return response()->json([
+                    'code' => '404',
+                    'message' => 'Not found'
+                ], 404);
+            }
+
+            return response()->json($result, 200);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+$router->get('v1/category/list', 'CategoryController@list');
+$router->get('v1/category/list/{id}', 'CategoryController@list');
+$router->post('v1/category/search_videos', 'CategoryController@search_videos');
+$router->get('v1/category/search_videos/{genre}', 'CategoryController@search_videos');
