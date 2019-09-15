3c1866f653e313917ac4ae25df7831034169ccf5
Aldy Prastyo <51155424+dipras@users.noreply.github.com>
2019-09-15 16:45:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->group(['prefix'=>'/conm/v1'], function() use($router){
-	$router->get('category/list', 'CategoryController@list');
-    $router->get('category/list/{id}', 'CategoryController@list');
-    $router->post('category/search_videos', 'CategoryController@search_videos');
-    $router->get('category/search_videos/{genre}', 'CategoryController@search_videos');
+$router->group(['prefix'=>'/conm/v1/category/'], function() use($router){
+  	$router->get('list', 'CategoryController@list');
+	$router->get('{id}', 'CategoryController@list');
+    	$router->post('search_videos', 'CategoryController@search_videos');
+});
+
+$router->group(['prefix' => '/conm/v1/'], function() use($router) {
+	$router->post('add', 'MetadataCategory@add');
+	$router->post('remove', 'MetadataCategory@remove');
 });
